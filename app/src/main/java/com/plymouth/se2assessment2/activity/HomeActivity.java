beaead7de2942bed5220fc9497f9269c464b044c
CalsonAo <93045571+CalsonAo@users.noreply.github.com>
2022-02-02 06:10:37 +0800

    create home activity

--- app/src/main/java/com/plymouth/se2assessment2/activity/HomeActivity.java
+++ app/src/main/java/com/plymouth/se2assessment2/activity/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.plymouth.se2assessment2;
+package com.plymouth.se2assessment2.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.plymouth.se2assessment2.R;
+
+public class HomeActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_home);
 	}
 }
