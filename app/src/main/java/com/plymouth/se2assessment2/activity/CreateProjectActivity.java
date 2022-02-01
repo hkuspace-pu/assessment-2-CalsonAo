7e761aebb5cea1af657c4e89ef829f9b555e7227
CalsonAo <93045571+CalsonAo@users.noreply.github.com>
2022-02-02 06:51:43 +0800

    create "create" and "list" activities

---
+++ app/src/main/java/com/plymouth/se2assessment2/activity/CreateProjectActivity.java
@@ -0,0 +1,16 @@
+package com.plymouth.se2assessment2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.plymouth.se2assessment2.R;
+
+public class CreateProjectActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_project);
+	}
+}
--- app/src/main/java/com/plymouth/se2assessment2/activity/HomeActivity.java
+++ app/src/main/java/com/plymouth/se2assessment2/activity/HomeActivity.java
@@ -2,7 +2,9 @@ package com.plymouth.se2assessment2.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.plymouth.se2assessment2.R;
 
@@ -12,5 +14,17 @@ public class HomeActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+	}
+
+	public void create(View view)
+	{
+		Intent intent = new Intent(this, CreateProjectActivity.class);
+		startActivity(intent);
+	}
+
+	public void list(View view)
+	{
+		Intent intent = new Intent(this, ListProjectActivity.class);
+		startActivity(intent);
 	}
 }
